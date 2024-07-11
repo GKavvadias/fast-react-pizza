e33c293205ffca1c3221577a451107377457653c
GKavvadias <il_gato_nero@hotmail.com>
2024-07-11 17:26:22 +0300

    Adding Tailwind CSS

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="flex items-center justify-between bg-stone-800 p-4 px-4 py-4 text-base text-stone-200 sm:px-6 md:text-sm">
+      <p className="space-x-4 text-stone-300 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
